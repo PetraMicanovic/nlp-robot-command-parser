a1f2d4d159b8b275c8f006e698334c01e3663c96
PetraMicanovic <micanovic.petra@yahoo.com>
2026-05-16 16:02:02 +0200

    Revert mBART to fp32 loading to resolve FP16 gradient unscale error

--- src/models/mbart_model.py
+++ src/models/mbart_model.py
@@ -34,10 +34,7 @@ def load_mbart_model(model_name, device):
     tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
     tokenizer.src_lang = MBART_SRC_LANG
 
-    if device == "cuda":
-        model = MBartForConditionalGeneration.from_pretrained(model_name,torch_dtype=torch.float16)
-    else:
-        model = MBartForConditionalGeneration.from_pretrained(model_name,torch_dtype=torch.float32)
+    model = MBartForConditionalGeneration.from_pretrained(model_name)
     model = model.to(device)
 
     n_params = 0
